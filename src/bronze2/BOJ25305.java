90b585f30ca7b5b8a216fb82587f12eb76736bae
giwoong <dev.choi0409@gmail.com>
2022-12-03 19:01:07 +0900

    feat:Add BOJ25305

---
+++ src/bronze2/BOJ25305.java
@@ -0,0 +1,24 @@
+package bronze2;
+
+// 커트라인
+// 첫째 줄에는 응시자의 수 N과 상을 받는 사람의 수 k가 공백을 사이에 두고 주어진다.
+// 둘째 줄에는 각 학생의 점수 x가 공백을 사이에 두고 주어진다.
+
+import java.util.*;
+
+public class BOJ25305 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        Integer x[] = new Integer[n];
+
+        for(int i = 0; i < n; i++){
+            x[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(x, Collections.reverseOrder());
+        System.out.println(x[k-1]);
+    }
+}
