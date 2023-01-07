8a62977c9920ab0b201895a7d11a53a0f9eee706
giwoong <dev.choi0409@gmail.com>
2023-01-07 18:10:06 +0900

    feat:Add 명예의전당(1)

---
+++ src/level1/명예의전당.java
@@ -0,0 +1,27 @@
+package src.level1;
+
+// level-1
+// 명예의 전당(1)
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> sc = new ArrayList();
+        for(int i = 0; i < score.length; i++) {
+            if(i < k - 1) {
+                sc.add(score[i]);
+                sc.sort(Collections.reverseOrder());
+                answer[i] = sc.get(sc.size() - 1);
+            }
+            else if(i >= k - 1) {
+                sc.add(score[i]);
+                sc.sort(Collections.reverseOrder());
+                answer[i] = sc.get(k - 1);
+            }
+        }
+
+        return answer;
+    }
+}
