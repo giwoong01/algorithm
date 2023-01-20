11a5bf10bab66736dd2e62c1d31404d155016417
giwoong <dev.choi0409@gmail.com>
2023-01-21 00:48:37 +0900

    feat:Add BOJ2751

---
+++ algorithm-baekjoon/src/silver5/BOJ2751.java
@@ -0,0 +1,32 @@
+package silver5;
+
+// 수 정렬하기 2
+// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+// 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BOJ2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int val : list) {
+            sb.append(val).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
