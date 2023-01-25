35d1fff7c591191e1c4a3e1f321e859b87e28278
giwoong <dev.choi0409@gmail.com>
2023-01-25 14:49:20 +0900

    feat:Add BOJ10870

---
+++ algorithm-baekjoon/src/bronze2/BOJ10870.java
@@ -0,0 +1,23 @@
+package bronze2;
+
+// 피보나치 수 5
+// f(n) = f(n - 1) + f(n + 1)
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
