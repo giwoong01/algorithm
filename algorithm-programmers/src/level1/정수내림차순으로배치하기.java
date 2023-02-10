74fcda9df614eba83e9b09ca56ae6971c037cc44
giwoong <dev.choi0409@gmail.com>
2023-02-10 11:59:49 +0900

    feat:Add level1

---
+++ algorithm-programmers/src/level1/정수내림차순으로배치하기.java
@@ -0,0 +1,24 @@
+차package src.level1;
+
+// level-1
+// 정수 내림차순으로 배치하기
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 정수내림차순으로배치하기 {
+    public long solution(long n) {
+        long answer = 0;
+        String[] str = Long.toString(n).split("");
+        Arrays.sort(str, Collections.reverseOrder());
+
+        String answ = "";
+
+        for (String s : str) {
+            answ += s;
+        }
+        answer = Long.parseLong(answ);
+
+        return answer;
+    }
+}
