39fb3accc83af112273618798dc2452fac0e809a
giwoong <dev.choi0409@gmail.com>
2023-02-18 16:34:10 +0900

    feat:Add level1

--- algorithm-programmers/src/level1/없는숫자더하기.java
+++ algorithm-programmers/src/level1/없는숫자더하기.java
@@ -1,11 +1,14 @@
 package src.level1;
 
+// level-1
+// 없는 숫자 더하기
+
 public class 없는숫자더하기 {
     public int solution(int[] numbers) {
         int answer = 45;
 
-        for (int i = 0; i < numbers.length; i++) {
-            answer -= numbers[i];
+        for (int number : numbers) {
+            answer -= number;
         }
         return answer;
     }
