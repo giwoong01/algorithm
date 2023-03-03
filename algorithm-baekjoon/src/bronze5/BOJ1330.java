f219d6a65a5fc03d7a21fecba50734725f722edb
giwoong <dev.choi0409@gmail.com>
2023-03-03 17:36:41 +0900

    feat:Add BOJ1330

---
+++ algorithm-baekjoon/src/bronze5/BOJ1330.java
@@ -0,0 +1,29 @@
+package bronze5;
+
+// 두 수 비교하기
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if ( A > B) {
+            System.out.println(">");
+        } else if ( A < B ) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
