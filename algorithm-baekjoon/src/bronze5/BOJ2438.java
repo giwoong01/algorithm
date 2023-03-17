160518a6057d342224d8fb67b23733c4afffd6f5
giwoong <dev.choi0409@gmail.com>
2023-03-17 23:20:10 +0900

    feat:Add BOJ2438

---
+++ algorithm-baekjoon/src/bronze5/BOJ2438.java
@@ -0,0 +1,23 @@
+package bronze5;
+
+// 별찍기 - 1
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
