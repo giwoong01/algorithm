03b0fac9825bd88529c17fe57cd66ecc15d7a731
giwoong <dev.choi0409@gmail.com>
2023-04-09 21:23:50 +0900

    feat:Add P120922

---
+++ algorithm-programmers/src/level0/P120922.java
@@ -0,0 +1,16 @@
+package src.level0;
+
+// level-0
+// 종이 자르기
+
+// 입출력 예
+// M	N	result
+// 2	2	3
+// 2	5	9
+// 1	1	0
+
+public class P120922 {
+    public int solution(int M, int N) {
+        return (M * N) - 1;
+    }
+}
