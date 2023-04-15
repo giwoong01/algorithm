58275824528dff3f3f4a918b9e9663ee7bdb5372
giwoong <dev.choi0409@gmail.com>
2023-04-16 01:28:20 +0900

    feat:Add P12977

---
+++ algorithm-programmers/src/level1/P12977.java
@@ -0,0 +1,39 @@
+package src.level1;
+
+// level-1
+// 소수 만들기
+
+// 입출력 예
+// nums	        result
+// [1,2,3,4]	1
+// [1,2,7,6,4]	4
+
+public class P12977 {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        boolean[] prime = new boolean[2998];
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (!prime[nums[i] + nums[j] + nums[k]]) {
+                        answer += 1;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
