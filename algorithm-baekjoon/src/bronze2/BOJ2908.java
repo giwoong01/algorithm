4ef5085907e12db1323bb2cd17448a07346b5d88
giwoong <dev.choi0409@gmail.com>
2023-04-26 15:36:25 +0900

    feat:Add BOJ2908

---
+++ algorithm-baekjoon/src/bronze2/BOJ2908.java
@@ -0,0 +1,19 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(Math.max(A, B));
+    }
+}
