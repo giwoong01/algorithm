c3fbf73375e5c1e27595824e01463b0081d86bd1
giwoong <dev.choi0409@gmail.com>
2023-05-03 00:04:03 +0900

    feat:Add Greedy1

---
+++ algorithm/src/chapter1/Greedy1.java
@@ -0,0 +1,19 @@
+package src.chapter1;
+
+// 거스름돈
+
+public class Greedy1 {
+    public static void main(String[] args) {
+        int n = 1260;
+        int count = 0;
+
+        int[] coinType = {500, 100, 50, 10};
+
+        for (int i = 0; i < coinType.length; i++) {
+            count += n / coinType[i];
+            n %= coinType[i];
+        }
+
+        System.out.println(count);
+    }
+}
