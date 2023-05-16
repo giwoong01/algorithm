a23f8c4a36a224c2281bf01597363376c2831972
giwoong <dev.choi0409@gmail.com>
2023-05-16 22:42:25 +0900

    feat:Add BOJ10250

---
+++ algorithm-baekjoon/src/bronze3/BOJ10250.java
@@ -0,0 +1,35 @@
+package bronze3;
+
+// ACM 호텔
+// 프로그램은 표준 입력에서 입력 데이터를 받는다. 프로그램의 입력은 T 개의 테스트 데이터로 이루어져 있는데 T 는 입력의 맨 첫 줄에 주어진다.
+// 각 테스트 데이터는 한 행으로서 H, W, N, 세 정수를 포함하고 있으며 각각 호텔의 층 수, 각 층의 방 수, 몇 번째 손님인지를 나타낸다(1 ≤ H, W ≤ 99, 1 ≤ N ≤ H × W).
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10250 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            if (N % H == 0) {
+                sb.append((H * 100) + N / H).append('\n');
+            } else {
+                sb.append(((N % H) * 100) + ((N / H) + 1)).append('\n');
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
