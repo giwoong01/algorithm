9dec8cc5ba55b3c1918e2e3157eaf0549e851e7d
giwoong <dev.choi0409@gmail.com>
2023-07-11 22:53:06 +0900

    feat:Add P136798

--- algorithm-programmers/src/level1/P136798.java
+++ algorithm-programmers/src/level1/P136798.java
@@ -1,19 +1,41 @@
 package src.level1;
 
-// level-1
-// 기사단원의 무기
+/*
+level-1
+기사단원의 무기
+
+입출력 예
+number	limit	power	result
+5	    3	    2	    10
+10	    3	    2	    21
+ */
 
 class P136798 {
-    public int solution(int number, int limit, int power) {
+    public static void main(String[] args) {
+        int number = 5;
+        int limit = 3;
+        int power = 2;
+
+        System.out.println(solution(number, limit, power));
+    }
+
+    public static int solution(int number, int limit, int power) {
         int answer = 0;
+
         for (int i = 1; i <= number; i++) {
             int count = 0;
+
             for (int j = 1; j * j <= i; j++) {
-                if (j * j == i) count++;
-                else if (i % j == 0) count += 2;
+                if (j * j == i) {
+                    count++;
+                } else if (i % j == 0) {
+                    count += 2;
+                }
             }
+
             answer += count > limit ? power : count;
         }
+
         return answer;
     }
 }
