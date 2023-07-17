e31e4c578c2d39d7c9b5622fe0d98962531dc85e
giwoong <dev.choi0409@gmail.com>
2023-07-17 11:27:23 +0900

    feat:Add P12919

--- algorithm-programmers/src/level1/P12919.java
+++ algorithm-programmers/src/level1/P12919.java
@@ -1,13 +1,22 @@
 package src.level1;
 
+/*
+level-1
+서울에서 김서 찾기
+
+입출력 예
+seoul	        return
+["Jane", "Kim"]	"김서방은 1에 있다"
+ */
+
 public class P12919 {
     public String solution(String[] seoul) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < seoul.length; i++) {
             if (seoul[i].equals("Kim"))
-                answer = "김서방은 " + i + "에 있다";
+                answer.append("김서방은 ").append(i).append("에 있다");
         }
-        return answer;
+        return answer.toString();
     }
 }
