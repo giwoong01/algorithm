fe6d3b511910f5f95e1ac8e19e04c998862c13da
giwoong <dev.choi0409@gmail.com>
2023-07-17 11:26:26 +0900

    feat:Add P135808

--- algorithm-programmers/src/level1/P135808.java
+++ algorithm-programmers/src/level1/P135808.java
@@ -6,32 +6,24 @@ package src.level1;
 import java.util.*;
 
 class P135808 {
-    public int solution(int k, int m, int[] score) {
+    public static void main(String[] args) {
+        int k = 3;
+        int m = 4;
+        int[] score = {1, 2, 3, 1, 2, 3, 1};
+
+        System.out.println(solution(k, m, score));
+    }
+
+    public static int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
 
         for (int i = score.length - 1; i >= 0; i--) {
-            if((score.length - i) % m == 0) answer += score[i] * m;
+            if((score.length - i) % m == 0) {
+                answer += score[i] * m;
+            }
         }
 
         return answer;
     }
 }
-
-/*
-import java.util.*;
-
-class 과일장수 {
-    public int solution(int k, int m, int[] score) {
-        int answer = 0;
-
-        Arrays.sort(score);
-
-        for(int i = score.length; i >= m; i -= m){
-            answer += score[i - m] * m;
-        }
-
-        return answer;
-    }
-}
- */
