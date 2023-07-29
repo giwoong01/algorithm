9a867cbb38f52d5fd121c34eb20a41d7fd790fd7
giwoong <dev.choi0409@gmail.com>
2023-07-29 14:01:08 +0900

    feat:Add BOJ10989

---
+++ algorithm-baekjoon/src/bronze1/BOJ10989.java
@@ -0,0 +1,37 @@
+package bronze1;
+
+/*
+수 정렬하기 3
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i : arr) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
