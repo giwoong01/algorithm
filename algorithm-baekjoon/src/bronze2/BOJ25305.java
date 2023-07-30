d35756463125489c7d1f7921fe3410d776580e73
giwoong <dev.choi0409@gmail.com>
2023-07-30 18:33:44 +0900

    feat:Add BOJ25305

--- algorithm-baekjoon/src/bronze2/BOJ25305.java
+++ algorithm-baekjoon/src/bronze2/BOJ25305.java
@@ -4,21 +4,27 @@ package bronze2;
 // 첫째 줄에는 응시자의 수 N과 상을 받는 사람의 수 k가 공백을 사이에 두고 주어진다.
 // 둘째 줄에는 각 학생의 점수 x가 공백을 사이에 두고 주어진다.
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class BOJ25305 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        Integer x[] = new Integer[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
 
-        for (int i = 0; i < n; i++) {
-            x[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        sc.close();
-        Arrays.sort(x, Collections.reverseOrder());
-        System.out.println(x[k - 1]);
+
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - k]);
     }
 }
