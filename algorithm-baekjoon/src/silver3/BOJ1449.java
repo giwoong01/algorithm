67c6a07a8090682d43d0150e13e3f4c2062b4a64
giwoong <dev.choi0409@gmail.com>
2023-09-04 22:06:05 +0900

    feat:Add BOJ1449

---
+++ algorithm-baekjoon/src/silver3/BOJ1449.java
@@ -0,0 +1,46 @@
+package silver3;
+
+/*
+수리공 항승
+
+입력
+첫째 줄에 물이 새는 곳의 개수 N과 테이프의 길이 L이 주어진다. 둘째 줄에는 물이 새는 곳의 위치가 주어진다. N과 L은 1,000보다 작거나 같은 자연수이고, 물이 새는 곳의 위치는 1,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 항승이가 필요한 테이프의 개수를 출력한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1449 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        double dif = arr[0] - 0.5;
+        int count = 1;
+        for (int j : arr) {
+            if (dif + L < j) {
+                count++;
+                dif = j - 0.5;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
