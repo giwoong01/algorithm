d78d83bcdd298b7e8e7b2e1010683ef112b6b301
giwoong <dev.choi0409@gmail.com>
2023-09-10 18:26:44 +0900

    feat:Add BOJ11729

---
+++ algorithm-baekjoon/src/silver1/BOJ11729.java
@@ -0,0 +1,34 @@
+package silver1;
+
+/*
+하노이 탑 이동 순서
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11729 {
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append((int) (Math.pow(2, N) - 1)).append("\n");
+        hanoiTop(N, 1, 2, 3);
+
+        System.out.println(sb);
+    }
+
+    public static void hanoiTop(int N, int a, int b, int c) {
+        if (N == 1) {
+            sb.append(a).append(" ").append(c).append("\n");
+        } else {
+            hanoiTop(N - 1, a, c, b);
+            sb.append(a).append(" ").append(c).append("\n");
+            hanoiTop(N - 1, b, a, c);
+        }
+    }
+}
