4af350c1d351fa22a91ff10d8b0d89b3046a8424
giwoong <dev.choi0409@gmail.com>
2023-12-23 01:42:54 +0900

    feat:Add P181868

---
+++ algorithm-programmers/src/level0/P181868.java
@@ -0,0 +1,25 @@
+package src.level0;
+
+/*
+level-0
+공백으로 구분하기 2
+
+입출력 예
+my_string	        result
+" i    love  you"	["i", "love", "you"]
+"    programmers  "	["programmers"]
+ */
+
+import java.util.Arrays;
+
+public class P181868 {
+    public static void main(String[] args) {
+        String my_string = " i    love  you";
+
+        System.out.println(Arrays.toString(solution(my_string)));
+    }
+
+    public static String[] solution(String my_string) {
+        return my_string.trim().split("\\s+");
+    }
+}
