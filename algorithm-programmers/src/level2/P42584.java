11093ef74ae4476a0df4e769a5fa43949321302f
giwoong <dev.choi0409@gmail.com>
2024-01-11 01:16:15 +0900

    feat:Add P42584

--- algorithm-programmers/src/level2/P42584.java
+++ algorithm-programmers/src/level2/P42584.java
@@ -19,6 +19,21 @@ public class P42584 {
         System.out.println(Arrays.toString(stackSolution(prices)));
     }
 
+    public static int[] forSolution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
     public static int[] stackSolution(int[] prices) {
         int[] answer = new int[prices.length];
         Stack<Integer> stack = new Stack<>();
