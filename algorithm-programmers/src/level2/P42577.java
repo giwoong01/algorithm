1c8591dd4fb1537def29d93fedf951f39cac582a
giwoong <dev.choi0409@gmail.com>
2024-01-15 23:51:44 +0900

    feat:Add P42577

--- algorithm-programmers/src/level2/P42577.java
+++ algorithm-programmers/src/level2/P42577.java
@@ -39,18 +39,4 @@ public class P42577 {
 
         return answer;
     }
-
-    public static boolean solutionArray(String[] phone_book) {
-        boolean answer = true;
-
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                answer = false;
-                break;
-            }
-        }
-
-        return answer;
-    }
 }
