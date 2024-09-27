8de37e0be2d83c0d03dc5d72635a8f51d312e095
giwoong <dev.choi0409@gmail.com>
2024-09-27 23:31:05 +0900

    feat:Add P181911

---
+++ algorithm-programmers/src/level0/P181911.java
@@ -0,0 +1,31 @@
+package src.level0;
+
+/*
+level-0
+부분 문자열 이어 붙여 문자열 만들기
+
+입출력 예
+my_strings	                                            parts	                            result
+["progressive", "hamburger", "hammer", "ahocorasick"]	[[0, 4], [1, 2], [3, 5], [7, 7]]	"programmers"
+ */
+
+import java.util.Arrays;
+
+public class P181911 {
+    public static void main(String[] args) {
+        String[] my_strings = {"progressive", "hamburger", "hammer", "ahocorasick"};
+        int[][] parts = {{0, 4}, {1, 2}, {3, 5}, {7, 7}};
+
+        System.out.println(solution(my_strings, parts));
+    }
+
+    public static String solution(String[] my_strings, int[][] parts) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < my_strings.length; i++) {
+            answer.append(my_strings[i], parts[i][0], parts[i][1] + 1);
+        }
+
+        return answer.toString();
+    }
+}
