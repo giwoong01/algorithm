2b248239ba2e88beef5a8231118d88efb5c5015b
giwoong <dev.choi0409@gmail.com>
2024-10-04 16:43:49 +0900

    feat:Add P340207

---
+++ algorithm-programmers/src/level1/P340207.java
@@ -0,0 +1,19 @@
+package src.level1;
+
+/*
+level-1
+[PCCE 기출문제] 1번 / 문자 출력
+
+빈칸 채우기는 이미 완성된 코드 중 빈칸에 알맞은 코드를 입력하는 문제 타입입니다.
+빈칸을 제외한 기본 코드는 수정할 수 없습니다.
+빈칸을 채우지 않을 경우, 실행 결과에 에러 메시지가 표시됩니다.
+ */
+
+public class P340207 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
