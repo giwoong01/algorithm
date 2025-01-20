4389ebe15288a28cce5d921a8226119645c8993f
giwoong <dev.choi0409@gmail.com>
2025-01-20 11:05:21 +0900

    feat:Add P120844

--- algorithm-programmers/src/level0/P120844.java
+++ algorithm-programmers/src/level0/P120844.java
@@ -26,15 +26,11 @@ public class P120844 {
         if (direction.equals("right")) {
             answer[0] = numbers[numbers.length - 1];
 
-            for (int i = 1; i < numbers.length; i++) {
-                answer[i] = numbers[i - 1];
-            }
+            System.arraycopy(numbers, 0, answer, 1, numbers.length - 1);
         } else {
             answer[numbers.length - 1] = numbers[0];
 
-            for (int i = 0; i < numbers.length - 1; i++) {
-                answer[i] = numbers[i + 1];
-            }
+            System.arraycopy(numbers, 1, answer, 0, numbers.length - 1);
         }
 
         return answer;
