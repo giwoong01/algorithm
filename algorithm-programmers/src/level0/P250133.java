ef1066cb62156c06be625ebc3cf2b3b3a72f9521
giwoong <dev.choi0409@gmail.com>
2025-07-16 09:30:33 +0900

    feat:Add P250133

---
+++ algorithm-programmers/src/level0/P250133.java
@@ -0,0 +1,23 @@
+package src.level0;
+
+/*
+level-0
+[PCCE 기출문제] 1번 / 출력
+
+출력 예시
+Spring is beginning
+13
+310
+ */
+
+public class P250133 {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
