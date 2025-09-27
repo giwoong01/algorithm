9ccd406a8435914fcc3d5068f36f072a3c2a97d7
giwoong <dev.choi0409@gmail.com>
2025-09-28 00:54:51 +0900

    feat:Add BOJ27866

---
+++ algorithm-baekjoon/src/bronze5/BOJ27866.java
@@ -0,0 +1,20 @@
+package bronze5;
+
+/*
+문자와 문자열
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ27866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        int index = Integer.parseInt(br.readLine());
+
+        System.out.println(input.charAt(index - 1));
+    }
+}
