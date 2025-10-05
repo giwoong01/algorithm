9ba48c544ae98d86ea0443a8d3ef583781adebab
giwoong <dev.choi0409@gmail.com>
2025-10-05 11:54:29 +0900

    feat:Add BOJ11718

---
+++ algorithm-baekjoon/src/bronze3/BOJ11718.java
@@ -0,0 +1,20 @@
+package bronze3;
+
+/*
+그대로 출력하기
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
