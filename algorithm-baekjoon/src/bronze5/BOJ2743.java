da1f135871118ea44c4b6b9b7b9ef4dccac25cbe
giwoong <dev.choi0409@gmail.com>
2025-10-11 22:53:46 +0900

    feat:Add BOJ2743

---
+++ algorithm-baekjoon/src/bronze5/BOJ2743.java
@@ -0,0 +1,16 @@
+package bronze5;
+
+/*
+단어 길이 재기
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
