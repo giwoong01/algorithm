99491c7cbeaba0e44105072892a616fd05fba570
giwoong <dev.choi0409@gmail.com>
2025-10-16 13:01:49 +0900

    feat:Add BOJ11654

---
+++ algorithm-baekjoon/src/bronze5/BOJ11654.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+/*
+아스키 코드
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+        System.out.println((int) c);
+    }
+}
