74ff339652032afdd374c99b1e8648f0ea5227c9
giwoong <dev.choi0409@gmail.com>
2025-12-14 15:41:00 +0900

    feat:add BOJ2156

--- algorithm-baekjoon/src/silver1/BOJ2156.java
+++ algorithm-baekjoon/src/silver1/BOJ2156.java
@@ -21,7 +21,9 @@ public class BOJ2156 {
 
         int[] dp = new int[n + 1];
         dp[1] = arr[1];
-        dp[2] = arr[1] + arr[2];
+        if (n > 1) {
+            dp[2] = arr[1] + arr[2];
+        }
         for (int i = 3; i <= n; i++) {
             dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
         }
