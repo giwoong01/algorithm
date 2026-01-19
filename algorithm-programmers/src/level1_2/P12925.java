be99ac296f99adcbaa67d7b5941e98968175fa4d
giwoong <dev.choi0409@gmail.com>
2026-01-19 17:13:38 +0900

    feat:add P12925_2

---
+++ algorithm-programmers/src/level1_2/P12925.java
@@ -0,0 +1,13 @@
+package src.level1_2;
+
+public class P12925 {
+    public static void main(String[] args) {
+        String s = "1234";
+
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
